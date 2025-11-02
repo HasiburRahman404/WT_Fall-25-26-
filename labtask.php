37b38d66143ffb6203ab387bec1c92f6681674a1
HasiburRahman404 <hasibur404rahman@gmail.com>
2025-11-02 12:35:53 +0600

    my first code

---
+++ labtask.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<head>
+    <title>Clinic Patient Registration</title>
+    </head>
+<body>
+    <center>
+    <h1 style="color:blueviolet">Clinic Patient Registration </h1>
+
+  
+
+    
+
+
+</center>
+<center>
+<table>
+    <tr>
+
+<td>Full Name:</td>
+</tr>
+<tr>
+   <td> <input type="text"></td>
+</tr>
+ <tr>
+  <td>  Age:</td>
+    </tr>
+    <tr>
+   <td> <input type="text"><br><td>
+</tr>
+<tr>
+   <td> Email Address:</td>
+</tr>
+<tr>
+   <td> <input type="text"><br></td>
+</tr>
+<tr>
+   <td> Insurance Provider:</td>
+</tr>
+<tr>
+  <td>   <select name="Select Provider">
+        <option value="">select provider</option><br>
+</select>
+</tr>
+<tr>
+   <td>Insurance Policy Number:</td>
+</tr>
+<tr>
+   <td> <input type="text"><br></td>
+</tr>
+</form>
+<tr>
+    <td>
+<center>
+    
+<h1 style="color:blueviolet">Additional Information</h1>
+
+</center></td>
+</tr>
+<tr>
+    <td>Username:</td>
+</tr>
+<tr>
+   <td> <input type="text"><br></td>
+</tr>
+<tr>
+   <td> Password:</td>
+</tr>
+<tr>
+  <td>  <input type="text"><br></td>
+</tr>
+<tr>
+  <td>  Confirm Password:</td>
+</tr>
+<tr>
+  <td>  <input type="text"><br></td>
+</tr>
+<tr>
+    <td>
+<center>
+   <input type="button" value="register" style="background-color:blueviolet; color:white;">
+</center></td>
+</tr>
+
+</center>
+
+</body>
