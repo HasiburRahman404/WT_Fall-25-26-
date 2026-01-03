6f68bf3f5b4ac2b3c87708fb727a0f71ab303f40
HasiburRahman404 <hasibur404rahman@gmail.com>
2026-01-04 01:43:05 +0600

    4 tarikh all done

---
+++ Project-AgroLink/Login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Customer Login - AgroLink</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+
+<div class="login-page">
+
+    <!-- LEFT GREEN SECTION -->
+    <div class="left-panel">
+        <div class="logo-container">
+            <img src="images/logo.png" alt="AgroLink Logo" class="logo">
+            <h1>AgroLink</h1>
+        </div>
+        <p>Connecting Farmers and Customers</p>
+    </div>
+
+    <!-- RIGHT LOGIN SECTION -->
+    <div class="right-panel">
+        <div class="container">
+            <h2>Login</h2>
+
+            <form>
+                <label>Email</label>
+                <input type="email" id="email" name="email">
+
+                <label>Password</label>
+                <input type="password" id="password" name="password">
+
+                <button type="submit" id="button">Login</button>
+
+                <p class="logintext">
+                    Don’t have an account? <a href="register.html">Register</a>
+                </p>
+            </form>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
